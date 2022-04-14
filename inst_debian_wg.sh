a24b84dadb566aeb796e75b6e819f24622f15b33
Yury Mankevich <byvnis@gmail.com>
2022-04-14 19:44:17 +0300

    Delete iptables old rules first

--- inst_debian_wg.sh
+++ inst_debian_wg.sh
@@ -8,6 +8,7 @@ FWDPORT=20000
 # -----
 systemctl stop wg-quick@wg0.service
 systemctl disable wg-quick@wg0.service
+/etc/wireguard/wg0_rules.sh -d
 rm /etc/sysctl.d/wg0.conf
 rm /etc/wireguard/wg0*
 sysctl --system
