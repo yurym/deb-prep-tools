3713d1f2b2c70709e39e35b7ec374c574d811749
Yury Mankevich <byvnis@gmail.com>
2022-04-14 20:05:08 +0300

    Checking previously installed instance first

--- inst_debian_wg.sh
+++ inst_debian_wg.sh
@@ -6,12 +6,14 @@ SERVERPORT=51194
 SERVERPORTALT=53,51820
 FWDPORT=20000
 # -----
-systemctl stop wg-quick@wg0.service
-systemctl disable wg-quick@wg0.service
-/etc/wireguard/wg0_rules.sh -d
-rm /etc/sysctl.d/wg0.conf
-rm /etc/wireguard/wg0*
-sysctl --system
+if [ -f /etc/wireguard/wg0_rules.sh ]; then
+  systemctl stop wg-quick@wg0.service
+  systemctl disable wg-quick@wg0.service
+  /etc/wireguard/wg0_rules.sh -d
+  rm /etc/sysctl.d/wg0.conf
+  rm /etc/wireguard/wg0*
+  sysctl --system
+fi
 # -----
 apt-get install -y \
   wireguard wireguard-tools \
