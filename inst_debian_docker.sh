dfa867cf6166f9b1add78ea15f9b70e1c54e97f9
Yury Mankevich <byvnis@gmail.com>
2023-03-31 13:54:21 +0300

    Update docker script

--- inst_debian_docker.sh
+++ inst_debian_docker.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-apt-get update
-apt-get -y install ca-certificates curl gnupg lsb-release
+# https://docs.docker.com/engine/install/debian
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+apt update
+apt -y install ca-certificates curl gnupg
+
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io
-
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 docker compose version
-
-#chmod -x `basename "$0"`
